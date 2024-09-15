c3ee9248f6cbd85a42baca3ac3d42fed36bde586
hemangjain17 <hemangjain_23cs174@dtu.ac.in>
2024-09-15 18:43:45 +0530

    Initial Commits

---
+++ sarva/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import Dashboard from './components/Dashboard';
+function App() {
+  return (
+    <Dashboard />
+  );
+}
+
+export default App;
---
+++ sarva/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+// src/components/Dashboard.js
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>SARVA</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
